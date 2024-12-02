6da1d512483be23e0b2e6bc309bcd93b1014ea55
Shashwat Chovatiya <shashwatchovatiya316@gmail.com>
2024-12-02 12:31:56 +0530

    first added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path"; // Import `path` correctly
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Add alias for `@repo-2`
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@repo-2": path.resolve(__dirname, "../repo-2/src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
